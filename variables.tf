88b468f23c101b92c786d0e240f61292ea27666a
lalithaa2407 <lalitalallu02@gmail.com>
2025-11-25 10:17:57 +0530

    removed backup policy

--- variables.tf
+++ variables.tf
@@ -119,15 +119,7 @@ variable "nsg_rules" {
   }
 }
 
-# azurerm_recovery_services_vault
-variable "recovery_services_vault_name" {
-  type        = string
-  description = "name of the recover service vault"
-}
-variable "services_vault_resource_group_name" {
-  type        = string
-  description = "name of resource group where the recovery service vault reside in"
-}
+
 
 # public IP
 variable "ip_version" {
